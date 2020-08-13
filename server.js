842e0a4bea3f3dc3fb2d1e115078a65bbf320504
keyuls <keyul@asu.edu>
2020-08-13 09:19:18 -0700

    try express

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ ghost()
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
   });
+
+
+  app.set('port', (process.env.PORT || 5000));
+
+  //For avoidong Heroku $PORT error
+  app.get('/', function(request, response) {
+      var result = 'App is running'
+      response.send(result);
+  }).listen(app.get('port'), function() {
+      console.log('App is running, server is listening on port ', app.get('port'));
+  }); 
